d07286337f055c2695868273d4e42d275d06ee8d
Gan Tu <tugan@berkeley.edu>
2017-04-14 13:12:20 -0700

    update Linear Node with backward method, add documentation

--- week2/miniflow.py
+++ week2/miniflow.py
@@ -50,7 +50,7 @@ class Input(Node):
         # The most important property on an Input is value.
         # self.value is set during `topological_sort` later.
         Node.__init__(self)
-        
+
     def forward(self, value=None):
         if value is not None:
             self.value = value
@@ -89,13 +89,13 @@ class Add(Node):
             self.value += n.value
 
 class Linear(Node):
-    def __init__(self, inputs, weights, bias):
-        Node.__init__(self, [inputs, weights, bias])
-
-        # NOTE: The weights and bias properties here are not
-        # numbers, but rather references to other nodes.
-        # The weight and bias values are stored within the
-        # respective nodes.
+    """
+    Represents a node that performs a linear transform.
+    """
+    def __init__(self, X, W, b):
+        # The base class (Node) constructor. Weights and bias
+        # are treated like inbound nodes.
+        Node.__init__(self, [X, W, b])
 
     def forward(self):
         """
@@ -105,6 +105,24 @@ class Linear(Node):
         W = self.inbound_nodes[1].value
         b = self.inbound_nodes[2].value
         self.value = np.dot(X, W) + b
+
+    def backward(self):
+        """
+        Calculates the gradient based on the output values.
+        """
+        # Initialize a partial for each of the inbound_nodes.
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_nodes}
+        # Cycle through the outputs. The gradient will change depending
+        # on each output, so the gradients are summed over all outputs.
+        for n in self.outbound_nodes:
+            # Get the partial of the cost with respect to this node.
+            grad_cost = n.gradients[self]
+            # Set the partial of the loss with respect to this node's inputs.
+            self.gradients[self.inbound_nodes[0]] += np.dot(grad_cost, self.inbound_nodes[1].value.T)
+            # Set the partial of the loss with respect to this node's weights.
+            self.gradients[self.inbound_nodes[1]] += np.dot(self.inbound_nodes[0].value.T, grad_cost)
+            # Set the partial of the loss with respect to this node's bias.
+            self.gradients[self.inbound_nodes[2]] += np.sum(grad_cost, axis=0, keepdims=False)
 
 class Sigmoid(Node):
     def __init__(self, node):
